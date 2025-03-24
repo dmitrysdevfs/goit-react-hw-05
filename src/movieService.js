7bebf8f1a796c32fe1b2f6c8eb29721ba4857908
Dmytro <d.salyakin@gmail.com>
2025-03-24 17:24:55 +0200

    Add fetchTrendingMovies function and basic integration on HomePage

---
+++ src/movieService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+const defaultLanguage = 'us-EN';
+
+const commonOptions = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${apiKey}`,
+  },
+};
+
+export const fetchTrendingMovies = async () => {
+  const params = {
+    language: defaultLanguage,
+  };
+
+  const resp = await axios.get(`${BASE_URL}trending/movie/day?`, {
+    ...commonOptions,
+    params,
+  });
+  return resp.data.results;
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from 'react';
+import { fetchTrendingMovies } from '../movieService';
+
 export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const data = await fetchTrendingMovies();
+        setMovies(data);
+      } catch {}
+    }
+
+    getMovies();
+  }, []);
+
   return <div>Home Page</div>;
 }
